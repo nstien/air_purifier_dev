771d91924d67af88585100cdad9a09f36e26df70
NguyenTien <nguyentien11071999@gmail.com>
2022-05-12 14:14:20 +0700

    update

--- src/app/models/model.js
+++ src/app/models/model.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const data = new Schema({
     power: String,
-    cod: String,
-    temperature: String,
-    co2: String,
-    humidity: String,
+    // cod: String,
+    // temperature: String,
+    // co2: String,
+    // humidity: String,
 }, {
     timestamps: true
 });
